2724ae295e78800cfe651e9066d92b4d09768f1b
bhoomikag203 <bhoomikag203@gmail.com>
2019-08-21 22:59:27 +0530

    tried on own

--- sorting/selection_sort.go
+++ sorting/selection_sort.go
@@ -4,26 +4,23 @@ import (
 	"fmt"
 )
 
+func main() {
+	list := []int{64, 25, 12, 22, 11}
+	fmt.Println(selection_sort(list))
+}
+
 func selection_sort(list []int) []int {
 	length := len(list)
-	for i := 0; i < length-1; i++ {
+	for i := 0; i < length; i++ {
 		min_index := i
 		for j := i + 1; j < length; j++ {
 			if list[j] < list[min_index] {
 				min_index = j
 			}
-
 		}
-
-		temp := list[i]
-		list[i] = list[min_index]
-		list[min_index] = temp
+		temp := list[min_index]
+		list[min_index] = list[i]
+		list[i] = temp
 	}
-
 	return list
-}
-
-func main() {
-	list := []int{64, 25, 12, 22, 11}
-	fmt.Println(selection_sort(list))
 }
